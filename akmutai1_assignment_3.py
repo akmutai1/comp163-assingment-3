e9a575f8b75ca5b3a6970844f7e2ffd90cbc710d
akmutai1 <akmutai1@aggies.ncat.edu>
2025-09-26 20:01:02 -0400

    Create akmutai1_assignment_3.py

---
+++ akmutai1_assignment_3.py
@@ -0,0 +1,12 @@
+student_name = "Adams Mutai"  
+current_gpa = 3.5
+study_hours = 4
+social_points = 76
+stress_level = 50
+
+print(f"Welcome {student_name}")
+print("Here are your stats:")
+print(f"GPA: {current_gpa}")
+print(f"Study Hours: {study_hours}")
+print(f"Social Points: {social_points}")
+print(f"Stress Level: {stress_level}")
